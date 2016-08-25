d32e2b1afb0f4f7773bfe32388add51a27f24752
Sten Roger Sandvik <srs@enonic.com>
2016-08-25 23:08:42 +0200

    Added router test. This fixes #20.

--- modules/purplejs-router/src/test/groovy/io/purplejs/router/itest/RouterLibTest.groovy
+++ modules/purplejs-router/src/test/groovy/io/purplejs/router/itest/RouterLibTest.groovy
@@ -62,4 +62,97 @@ class RouterLibTest
         'GET'    | '/a/b' | Status.OK        | 'GET /a/b'
         'DELETE' | '/a/b' | Status.OK        | '* /a/b'
     }
+
+    def "route with path params"()
+    {
+        setup:
+        script( '''
+            var router = require('/lib/router')();
+
+            router.get('/a/{id}', function(req) {
+                return {
+                    body: 'GET /a/' + req.pathParams.id
+                };
+            });
+
+            exports.service = function(req) {
+                return router.dispatch(req);
+            };
+        ''' );
+
+        when:
+        this.requestBuilder.method( 'GET' );
+        this.requestBuilder.uri( 'http://localhost:8080/a/12' );
+        def res = serve();
+
+        then:
+        res.status == Status.OK;
+        toStringBody( res ) == 'GET /a/12';
+    }
+
+    def "only filters"()
+    {
+        setup:
+        script( '''
+            var router = require('/lib/router')();
+
+            router.filter(function(req, next) {
+                var res = next(req);
+                res.body = 'filter1,' + res.body;
+                return res;
+            });
+
+            router.filter(function(req, next) {
+                return {
+                    body: 'filter2'
+                };
+            });
+
+            exports.service = function(req) {
+                return router.dispatch(req);
+            };
+        ''' );
+
+        when:
+        this.requestBuilder.method( 'GET' );
+        this.requestBuilder.uri( 'http://localhost:8080/a/12' );
+        def res = serve();
+
+        then:
+        res.status == Status.OK;
+        toStringBody( res ) == 'filter1,filter2';
+    }
+
+    def "filter and route"()
+    {
+        setup:
+        script( '''
+            var router = require('/lib/router')();
+
+            router.filter(function(req, next) {
+                var res = next(req);
+                res.body = 'filter,' + res.body;
+                return res;
+            });
+
+            router.get('/a', function(req) {
+                return {
+                    body: 'route'
+                };
+            });
+
+            exports.service = function(req) {
+                return router.dispatch(req);
+            };
+        ''' );
+
+        when:
+        this.requestBuilder.method( 'GET' );
+        this.requestBuilder.uri( 'http://localhost:8080/a' );
+        def res = serve();
+
+        then:
+        res.status == Status.OK;
+        toStringBody( res ) == 'filter,route';
+    }
 }
