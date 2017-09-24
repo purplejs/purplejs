ca5425e47fcb584ef7f3a54d7bb91c041e242984
Sten Roger Sandvik <srs@enonic.com>
2017-09-25 01:11:03 +0200

    Implemented ResourceResult.

--- modules/purplejs-core/src/main/java/io/purplejs/core/resource/ResourceResolver.java
+++ modules/purplejs-core/src/main/java/io/purplejs/core/resource/ResourceResolver.java
@@ -1,5 +1,16 @@
 package io.purplejs.core.resource;
 
+/**
+ * TODO:
+ *
+ * Maybe it's best to let this be internal and have some sort of resolver-settings
+ * (io.purplejs.resource.*). The settings could include the following:
+ *
+ * - search-paths (/lib) for used when the resource is specified like "a/b/c".
+ * - root-paths (/) for used with /site etc.
+ *
+ * If specified by "a/b/c" and not found, it could revert back to "./a/b/c" by default.
+ */
 public interface ResourceResolver
 {
     ResourceResult resolve( ResourcePath base, String path );
